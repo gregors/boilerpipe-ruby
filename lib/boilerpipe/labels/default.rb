78bc2441d4d999f05ec7e721b20ba5f260cab118
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2016-11-08 00:32:45 -0500

    add default labels

---
+++ lib/boilerpipe/labels/default.rb
@@ -0,0 +1,17 @@
+module Boilerpipe::Labels
+  module Default
+    :TITLE
+    :ARTICLE_METADATA
+    :INDICATES_END_OF_TEXT
+    :MIGHT_BE_CONTENT
+    :VERY_LIKELY_CONTENT
+    :STRICTLY_NOT_CONTENT
+    :HR
+    :LI
+    :HEADING
+    :H1
+    :H2
+    :H3
+    MARKUP_PREFIX = '<'
+  end
+end
---
+++ spec/labels/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module Boilerpipe::Labels
+  describe Default do
+   it 'has markup prefix' do
+     expect(Boilerpipe::Labels::Default::MARKUP_PREFIX).to eq '<'
+   end
+  end
+end
