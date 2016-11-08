26c461218626f654955a6c0faa171b530d0ae7d5
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2016-11-08 01:58:24 -0500

    terminating_blocks_finder logic

---
+++ lib/boilerpipe/filters/terminating_blocks_finder.rb
@@ -0,0 +1,36 @@
+module Boilerpipe::Filters
+  class TerminatingBlocksFinder
+    def process(doc)
+      changes = false
+      doc.text_blocks.each do |text_block|
+        if text_block.num_words < 15
+          text = text_block.text.trim.downcase
+          if text.length >= 8
+            text_block.labels << :INDICATES_END_OF_TEXT if finds_match?(text)
+            #seems weird that only this block sets changes to true - bug???
+            changes = true
+          elsif tb.link_density == 1.0
+            text_block.labels << :INDICATES_END_OF_TEXT if text == 'comment'
+          end
+        end
+      end
+      changes
+    end
+
+    def finds_match?(text)
+       text.start_with?('comments') ||
+       text =~ /^\d+ (comments|users responded in)/ || # starts with number
+       text.start_with?('© reuters') ||
+       text.start_with?('please rate this') ||
+       text.start_with?('post a comment') ||
+       text.include?('what you think...') ||
+       text.include?('add your comment') ||
+       text.include?('add comment') ||
+       text.include?('reader views') ||
+       text.include?('have your say') ||
+       text.include?('reader comments') ||
+       text.include?('rätta artikeln') ||
+       text == 'thanks for your comments - this feedback is now closed'
+    end
+  end
+end
---
+++ spec/filters/terminating_blocks_finder_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+module Boilerpipe::Filters
+  describe TerminatingBlocksFinder do
+    describe '#process' do
+      it 'requires a text document'
+      context 'the text is ending text' do
+        it 'returns true'
+        it 'sets a label on the document'
+      end
+
+      context 'the text is not ending text' do
+        it 'returns false'
+        it 'doesnt change the document'
+      end
+    end
+  end
+end
