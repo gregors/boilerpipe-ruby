2b84993fda86a2a4f8b70b8f23be82d3c44ba3ca
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2016-12-09 16:44:32 -0500

    removing broken tags

--- lib/boilerpipe/sax/run_parser.rb
+++ lib/boilerpipe/sax/run_parser.rb
@@ -5,7 +5,7 @@ require_relative './parser'
 file = File.open('../../../spec/fixtures/parsing-big-xml-files-with-nokogiri.html')
 puts file
 
-noko = Nokogiri::XML::SAX::Parser.new(Parser.new)
+noko = Nokogiri::HTML::SAX::Parser.new(Parser.new)
 puts noko
 
 puts noko.parse(file)
