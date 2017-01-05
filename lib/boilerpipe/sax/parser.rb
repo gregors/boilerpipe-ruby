46b3ca1d84dc5ece1dd083484df7f9f88ecef657
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-01-05 12:39:33 -0500

    add start_element logic for parser

--- lib/boilerpipe/sax/parser.rb
+++ lib/boilerpipe/sax/parser.rb
@@ -1,16 +1,32 @@
 class Parser < Nokogiri::XML::SAX::Document
+
+  def initialize
+    @label = []
+    @tag_actions = {}
+    @tag_level = 0
+	end
+
   def start_element(name, attrs = [])
     puts "start element #{name}"
-    # Handle each element, expecting the name and any attributes
+    @label << nil
+    ta = @tag_actions[name]
+
+    if ta.nil?
+      @tag_level += 1
+      @flush = true
+		else
+			@tag_level += 1 if ta.changes_tag_level
+      @flush = ta.start(self, name, name, attrs) | flush
+		end
+
+    @last_event = :START_TAG
+    @last_start_tag = name
   end
 
   def characters(string)
-    #puts "character #{string}"
-    # Any characters between the start and end element expected as a string
   end
 
   def end_element(name)
     puts "end element #{name}"
-    # Given the name of an element once its closing tag is reached
   end
 end
