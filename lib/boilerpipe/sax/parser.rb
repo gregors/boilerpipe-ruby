ac4d2b89ec3c6dd66a837dcc8d911b3fc5dc479b
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-01-07 13:48:07 -0500

    add characters to parser

--- lib/boilerpipe/sax/parser.rb
+++ lib/boilerpipe/sax/parser.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'set'
 
 class Parser < Nokogiri::XML::SAX::Document
 
@@ -6,6 +7,10 @@ class Parser < Nokogiri::XML::SAX::Document
     @label = []
     @tag_actions = {}
     @tag_level = 0
+    @sb_last_was_whitespace = false
+    @current_contained_text_elements = Set.new
+    @text_buff = ''
+    @token_buff = ''
 	end
 
   def start_element(name, attrs = [])
@@ -26,6 +31,62 @@ class Parser < Nokogiri::XML::SAX::Document
   end
 
   def characters(string)
+    @text_element_idx += 1
+    if @flush
+      flush_block
+      @flush = false
+    end
+
+    return if @in_ignoreable_element != 0
+    return if string.length == 0
+
+    start_whitespace = false
+    endt_whitespace = false
+
+    # replace all whitespace with simple space
+    string = string.gsub(/\s+/, ' ')
+
+    # trim whitespace
+    start_whitespace = string  =~ /^\s/
+    end_whitespace = string  =~ /\s$/
+    string = string.strip
+
+    #  append leading whitespace if previous wasn't already one
+    if string.size == 0
+      if startWhitespace || endWhitespace
+        if ! @sb_last_was_whitespace
+          @text_buff << ' '
+          @token_buff << ' '
+        end
+        @sb_last_was_whitespace = true
+      else
+        @sb_last_was_whitespace = false
+      end
+      @last_event = :WHITESPACE
+      return
+    end
+
+    if start_whitespace
+      if ! @sb_last_was_whitespace
+        @text_buff << ' '
+        @token_buff << ' '
+      end
+    end
+
+    # set block levels
+    @block_tag_level = @tag_level if @block_tag_level == -1
+    @text_buff << string
+    @token_buff <<  string
+
+    # add trailing space
+    if end_whitespace
+      @text_buff << ' '
+      @token_buff << ' '
+    end
+
+    @sb_last_was_whitespace = end_whitespace
+    @last_event = :CHARACTERS
+    @current_contained_text_elements << text_element_idx
   end
 
   def end_element(name)
