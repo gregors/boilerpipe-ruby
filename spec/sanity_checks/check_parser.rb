e5002a14fd402e1e59807a1c85ca36d3cdd52d26
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-01-08 18:41:51 -0500

    add check_parser sanity check

---
+++ spec/sanity_checks/check_parser.rb
@@ -0,0 +1,27 @@
+require_relative './jars/boilerpipe-common-2.0-SNAPSHOT-jar-with-dependencies.jar'
+
+java_import 'com.kohlschutter.boilerpipe.extractors.ArticleExtractor'
+java_import 'com.kohlschutter.boilerpipe.sax.BoilerpipeHTMLParser'
+java_import 'org.xml.sax.InputSource'
+java_import java.io.StringReader
+
+file_path = '../fixtures/parsing-big-xml-files-with-nokogiri.html'
+file_contents = File.read(file_path)
+
+# parse html and return text document
+# return getText(new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument());
+@parser = BoilerpipeHTMLParser.new
+@string_reader = StringReader.new(file_contents)
+@is = InputSource.new(@string_reader)
+@parser.parse(@is);
+@doc = @parser.to_text_document
+#puts @doc.debug_string
+
+puts "number of text blocks: #{@doc.text_blocks.size}"
+
+#@ruby_parser = Parser.new
+#@rubr_parser.parse(file_contents)
+#@ruby_doc = @ruby_parser.text_document
+#puts "number of text blocks: #{@ruby_doc.text_blocks.size}"
+
+puts 'done'
