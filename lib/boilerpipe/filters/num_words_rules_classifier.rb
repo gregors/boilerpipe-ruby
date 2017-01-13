3a385bec4ee21ef60806ecb1eeca1a953952c69c
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-01-12 23:22:09 -0500

    stub NumWordsRulesClassifier

---
+++ lib/boilerpipe/filters/num_words_rules_classifier.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+require 'set'
+module Boilerpipe::Filters
+  class NumWordsRulesClassifier 
+
+    def process(doc)
+    end
+
+    private
+    def classify(prev, current, nxt)
+    end 
+
+  end
+end
---
+++ spec/filters/num_words_rules_classifier_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module Boilerpipe::Filters
+  describe NumWordsRulesClassifier do
+    describe '#process' do
+      it 'processes a text document'
+    end
+  end
+end
