b003f11b460e49324710dff54182c29b60e76bec
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-01-13 23:09:40 -0500

    hook up parser, random fixes

--- lib/boilerpipe/sax/boilerpipe_html_parser.rb
+++ lib/boilerpipe/sax/boilerpipe_html_parser.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 module Boilerpipe::SAX
   class BoilerpipeHTMLParser
     def self.parse(text)
-      handler = BoilerpipeHTMLContentHandler.new
-      noko_parser = Nokogiri::HTML::SAX::Parser.new(@handler)
+      handler = HTMLContentHandler.new
+      noko_parser = Nokogiri::HTML::SAX::Parser.new(handler)
       noko_parser.parse(text)
       handler.text_document
     end
