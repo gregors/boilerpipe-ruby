11b2f7dc0428e8982e1ea37a7cb7419c48ce089d
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-02-13 21:38:29 -0500

    add block_tag_label tag action

---
+++ lib/boilerpipe/sax/tag_actions/block_tag_label.rb
@@ -0,0 +1,22 @@
+module Boilerpipe::SAX::TagActions
+# for block-level elements, which triggers some LabelAction on
+# the generated TextBlock.
+  class BlockTagLabel
+    def initialize(label_action)
+      @label_action = label_action
+    end
+
+    def start(handler, name, attrs)
+      handler.add_label_action(@label_action)
+      true
+    end
+
+    def end_tag(handler, name)
+      true
+    end
+
+    def changes_tag_level
+      true
+    end
+  end
+end
