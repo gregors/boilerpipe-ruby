4cb7f035acb38b0fb384cf8f3718faf513451dd4
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-02-18 19:17:44 -0500

    text block content sanity check

---
+++ spec/sanity_checks/original_text_block_content.rb
@@ -0,0 +1,24 @@
+require_relative './jars/boilerpipe-common-2.0-SNAPSHOT-jar-with-dependencies.jar'
+
+java_import 'com.kohlschutter.boilerpipe.extractors.ArticleExtractor'
+java_import 'com.kohlschutter.boilerpipe.sax.BoilerpipeHTMLParser'
+java_import 'org.xml.sax.InputSource'
+java_import java.io.StringReader
+
+file_path = '../fixtures/parsing-big-xml-files-with-nokogiri.html'
+file_contents = File.read(file_path)
+
+# parse html and return text document
+#puts 'java version'
+@parser = BoilerpipeHTMLParser.new
+@string_reader = StringReader.new(file_contents)
+@is = InputSource.new(@string_reader)
+@parser.parse(@is);
+@doc = @parser.to_text_document
+
+puts "number of text blocks: #{@doc.text_blocks.size}"
+@doc.text_blocks.each do |tb|
+  puts tb.text
+end
+
+puts 'done'
---
+++ spec/sanity_checks/ruby_text_block_content.rb
@@ -0,0 +1,14 @@
+require 'boilerpipe'
+
+file_path = '../fixtures/parsing-big-xml-files-with-nokogiri.html'
+file_contents = File.read(file_path)
+
+#puts 'ruby version'
+
+@ruby_doc = Boilerpipe::SAX::BoilerpipeHTMLParser.parse(file_contents)
+
+puts "number of text blocks: #{@ruby_doc.text_blocks.size}"
+@ruby_doc.text_blocks.each do |tb|
+  puts tb.text
+end
+puts 'done'
