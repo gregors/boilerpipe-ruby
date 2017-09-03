032203b41e0e815467f88daee1a3f74567db25be
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-02 20:25:38 -0400

    fix bad tags using nokogiri

--- lib/boilerpipe/sax/boilerpipe_html_parser.rb
+++ lib/boilerpipe/sax/boilerpipe_html_parser.rb
@@ -2,6 +2,10 @@ require 'nokogiri'
 module Boilerpipe::SAX
   class BoilerpipeHTMLParser
     def self.parse(text)
+
+      # use nokogiri to fix any bad tags, errors - keep experimenting with this
+      text = Nokogiri::XML(text).to_xml
+
       handler = HTMLContentHandler.new
       noko_parser = Nokogiri::HTML::SAX::Parser.new(handler)
       noko_parser.parse(text)
