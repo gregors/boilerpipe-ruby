28c6554576e5a1c335f09ff59d9b1a1c09a14732
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-02 23:33:49 -0400

    nokogiri - libxml - &nbsp bug workaround

--- lib/boilerpipe/sax/boilerpipe_html_parser.rb
+++ lib/boilerpipe/sax/boilerpipe_html_parser.rb
@@ -2,9 +2,12 @@ require 'nokogiri'
 module Boilerpipe::SAX
   class BoilerpipeHTMLParser
     def self.parse(text)
+      # nokogiri uses libxml for mri and nekohtml for jruby
+      # mri doesn't remove &nbsp; when missing the semicolon
+      text = text.gsub(/(&nbsp) /, '\1; ')
 
       # use nokogiri to fix any bad tags, errors - keep experimenting with this
-      text = Nokogiri::XML(text).to_xml
+      text = Nokogiri::HTML(text).to_html
 
       handler = HTMLContentHandler.new
       noko_parser = Nokogiri::HTML::SAX::Parser.new(handler)
