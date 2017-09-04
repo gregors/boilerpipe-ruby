fe5d535275c1083ffd17ec0f438e5183549d1ee3
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-04 18:26:11 -0400

    add sanity_check terminating_blocks_finder

---
+++ spec/sanity_checks/terminating_blocks_finder_check.rb
@@ -0,0 +1,11 @@
+require 'boilerpipe'
+
+#id = 1024
+(1).upto(1024) do |id|
+  file_contents = File.read("scraped/#{id}")
+  doc = Boilerpipe::SAX::BoilerpipeHTMLParser.parse(file_contents)
+  Boilerpipe::Filters::TerminatingBlocksFinder.process(doc)
+  tb = doc.text_blocks.select{|tb| tb.has_label?(:INDICATES_END_OF_TEXT) }
+  puts "end of text size: #{tb.size} has title id: #{id}" if tb.size > 0
+  #puts doc.debug_string
+end
