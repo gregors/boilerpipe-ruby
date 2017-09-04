4bbf72554e115f503a20e7d322ea8dcd0b72d8b5
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-04 17:04:14 -0400

    add heuristic_filter_base

---
+++ lib/boilerpipe/filters/heuristic_filter_base.rb
@@ -0,0 +1,7 @@
+module Boilerpipe::Filters
+  class HeuristicFilterBase
+    def self.num_full_text_words(tb, min_text_density=9.0)
+      tb.text_density >= min_text_density ? tb.num_words : 0
+    end
+  end
+end
