33299f67487585f1fcdc755a9050eb224cb18a55
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-04 16:43:04 -0400

    add another spec for num_words_fules_classifier

--- spec/filters/num_words_rules_classifier_spec.rb
+++ spec/filters/num_words_rules_classifier_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 module Boilerpipe::Filters
   describe NumWordsRulesClassifier do
+    let(:tb) { Boilerpipe::Document::TextBlock }
+    let(:doc) { Boilerpipe::Document::TextDocument.new '', text_blocks }
+
     describe '#process' do
       it 'processes a text document'
       context 'with a content text block' do
-        let(:tb) { Boilerpipe::Document::TextBlock }
         let(:text_blocks) do
           [
             tb.new('What a great day!'),
@@ -13,9 +15,6 @@ module Boilerpipe::Filters
           ]
         end
 
-        let(:doc) do
-          Boilerpipe::Document::TextDocument.new '', text_blocks
-        end
 
         it 'labels the text block as content' do
           NumWordsRulesClassifier.process(doc)
@@ -24,7 +23,17 @@ module Boilerpipe::Filters
       end
 
       context 'with a non-content text block' do
-        it 'labels the text block as non-content'
+        let(:text_blocks) do
+          [
+            tb.new('What a great day!'),
+            tb.new('What a great day!', 4)
+          ]
+        end
+
+        it 'labels the text block as non-content' do
+          NumWordsRulesClassifier.process(doc)
+          expect(doc.text_blocks.first.is_content?).to be false
+        end
       end
     end
   end
